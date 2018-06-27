760e21ac16d70d0cc36b81f412f8c98f08b536a1
tteguayco <teguayco95@gmail.com>
2018-06-27 10:06:32 +0000

    File dropping working

--- js/schema.js
+++ js/schema.js
@@ -1,37 +1,66 @@
 
+function loadFileContentInDOM(filename, file) {
+	// Place the name of the selected file inside the dropzone
+	if (filename.length > 0) {
+		$("#selected-filename").html("<b>" + filename + "<b/>");
+	}
+
+	// Set hidden the content of the file in the DOM
+	if (file) {
+		var reader = new FileReader();
+	    reader.readAsText(file, "UTF-8");
+
+	    reader.onload = function (evt) {
+	    	var fileContent = evt.target.result;
+	        $("#db-definition-file-content").text(fileContent);
+	    }
+
+	    reader.onerror = function (evt) {
+	        console.err("Error loading database definition file.");
+	    }
+	}
+}
+
 /**
  * Upload a file in the dropzone using a file chooser.
  */
-function selectFile() {
+function loadSelectedFile() {
 
 	document.getElementById("filechooser").click();
 	document.getElementById("filechooser").onchange = function() {
   		var fileFullPath = this.value;
   		var splittedPath = fileFullPath.split('\\');
   		var filename = splittedPath[splittedPath.length - 1];
-
-  		// Place the name of the selected file inside the dropzone
-  		$("#selected-filename").text(filename);
-
-  		// Set hidden the content of the file in the DOM
   		var file = document.getElementById("filechooser").files[0];
-  		if (file) {
-  			var reader = new FileReader();
-		    reader.readAsText(file, "UTF-8");
 
-		    reader.onload = function (evt) {
-		    	var fileContent = evt.target.result;
-		        $("#db-definition-file-content").text(fileContent);
-		    }
-
-		    reader.onerror = function (evt) {
-		        console.err("Error loading database definition file.");
-		    }
-  		}
+	  	loadFileContentInDOM(filename, file);
 	};
+}
+
+/**
+ * Load the dropped file inside the DOM (hidden)
+ */
+function loadDroppedFile(e) {
+	var filename = e.originalEvent.dataTransfer.files[0].name;
+	var file = e.originalEvent.dataTransfer.files[0];
+
+	loadFileContentInDOM(filename, file);
+}
+
+function handleDragOver(e) {
+	e.stopPropagation();
+    e.preventDefault();
+
+
 }
 
 $(document).ready(function() {
 
-	$("#dropzone").click(selectFile);
+	// Prevent the browser to open a file dropped inside in the window
+	window.addEventListener("dragover", function(e) { e = e || event; e.preventDefault(); }, false);
+	window.addEventListener("drop", function(e) { e = e || event; e.preventDefault(); }, false);
+
+	$("#dropzone").click(loadSelectedFile);
+	$("#dropzone").on("dragover", handleDragOver);
+	$("#dropzone").on("drop", loadDroppedFile);
 });
