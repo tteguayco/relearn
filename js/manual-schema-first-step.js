5f40dcbfa22f15607372f70ad4d838b0bbb94496
tteguayco <teguayco95@gmail.com>
2018-06-28 15:54:52 +0000

    Errors checking in the first step of schema manual definition (client-side)

--- js/manual-schema-first-step.js
+++ js/manual-schema-first-step.js
@@ -14,14 +14,79 @@ function removeLatestRelationNameTextBox() {
 	else {
 		// Display alert in the top left corner
 		$.uiAlert({
-			textHead: 'Relation name removal not allowed',
-			text: ' You need to provide at least one relation to your schema. ',
+			textHead: 'Relation removal not allowed',
+			text: ' You need to provide at least one relation in your schema. ',
 			bgcolor: '#DB2828',
 			textcolor: '#fff',
 			position: 'top-left',
 			icon: 'remove circle',
-			time: 5
+			time: 3
 		});
+	}
+}
+
+function isValidName(aName) {
+	var valid = true;
+
+	// Empty name
+	if (aName.length < 1) {
+		valid = false;
+	}
+
+	// The first character of the name is different from a letter
+	if (!aName.charAt(0).match(/[a-z]/i)) {
+		valid = false;
+	}
+
+	// The name contains spaces
+	if (aName.indexOf(' ') >= 0) {
+		valid = false;
+	}
+
+	return valid;
+}
+
+function checkFields() {
+	var errors = false;
+
+	// Database name
+	var databaseName = $("#db-name").val();
+	if (!isValidName(databaseName)) {
+		errors = true;
+	}
+	
+	// Relations names
+	var textBoxes = $(".rel-name");
+	var relationsNames = [];
+	var auxName;
+
+	// Collect non-empty relation names
+	for (i = 0; i < textBoxes.length; i++) {
+		auxName = textBoxes[i].value.trim();
+		if (auxName.length > 0) {
+			relationsNames.push(auxName);
+		}
+	}
+
+	// At least one relation name must be non-empty
+	if (relationsNames.length > 0) {
+		for (i = 0; i < relationsNames.length; i++) {
+			if (!isValidName(relationsNames[i])) {
+				errors = true;
+				break;
+			}
+		}
+	} else {
+		errors = true;
+	}
+
+	// Display info errors panel
+	if (errors) {
+		$("#errors-modal-first-step").show();
+	}
+	
+	else {
+		alert("No errors..");
 	}
 }
 
@@ -29,4 +94,12 @@ $(document).ready(function() {
 
 	$("#add-relation-btn").click(addRelationNameTextBox);
 	$("#remove-relation-btn").click(removeLatestRelationNameTextBox);
+	$("#first-step-next-btn").click(checkFields);
+
+	// To prevent a modal action from causing the modal to close
+	$('.modal').modal({
+		onApprove: function () {
+			return false;
+		}
+	});
 });
