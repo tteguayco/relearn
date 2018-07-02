d3a6deae56e84c7d35fe2291bdc7b3257dac0211
tteguayco <teguayco95@gmail.com>
2018-07-02 14:22:33 +0000

    Add/remove buttons functionality

--- js/manual-schema-second-step.js
+++ js/manual-schema-second-step.js
@@ -1,4 +1,57 @@
 
+function getAttributeGroup() {
+	return $('<div class="attribute-group"></div>');
+}
+
+function getAttributeNameTextBox() {
+	return $('<input type="text" class="attribute-name" placeholder="Attribute Name"></input>');
+}
+
+function getDomainsDropdown() {
+	var attributeDomainDropdown = $('<select class="ui dropdown attribute-domain"></select>');
+
+	// Domain dropdown options
+	attributeDomainDropdown.append('<option value="0">String</option>');
+	attributeDomainDropdown.append('<option value="1">Char</option>');
+	attributeDomainDropdown.append('<option value="2">Integer</option>');
+	attributeDomainDropdown.append('<option value="3">Float</option>');
+	attributeDomainDropdown.append('<option value="4">Date</option>');
+
+	return attributeDomainDropdown;
+}
+
+function addAttribute() {
+	var attributeList = $(this).siblings('.attribute-list');
+	var newAttributeGroup = getAttributeGroup();
+
+	attributeList.append(newAttributeGroup);
+	newAttributeGroup.append(getAttributeNameTextBox());
+	newAttributeGroup.append(getDomainsDropdown());
+}
+
+function removeAttribute() {
+	var attributeList = $(this).siblings('.attribute-list');
+	var numOfAttributes = attributeList.children('.attribute-group').length;
+	
+	// Remove last attribute
+	if (numOfAttributes > 1) {
+		attributeList.children()[numOfAttributes - 1].remove();
+	}
+
+	// Display alert: there must be at leaste one attribute
+	else {
+		$.uiAlert({
+			textHead: 'Attribute removal not allowed',
+			text: ' All the relations must have at least one attribute. ',
+			bgcolor: '#DB2828',
+			textcolor: '#fff',
+			position: 'top-left',
+			icon: 'remove circle',
+			time: 3
+		});
+	}
+}
+
 function prepareSecondModal() {
 	var databaseName = $("#db-name").text();
 	var relationsList = $(".rel-name").map(function(i, elem) { if (elem.value.length > 0) { return elem.value } });
@@ -35,18 +88,14 @@ function prepareSecondModal() {
 		accordionContent.append(addButton);
 		accordionContent.append(removeButton);
 
+		addButton.click(addAttribute);
+		removeButton.click(removeAttribute);
+
 		// Attribute names and domains
 		var segment = $('<div class="ui segment attribute-list"></div>');
-		var attributeGroup = $('<div class="attribute-group"></div>');
-		var attributeNameTextbox = $('<input type="text" class="attribute-name" placeholder="Attribute Name"></input>');
-		var attributeDomainDropdown = $('<select class="ui dropdown attribute-domain"></select>');
-
-		// Domain dropdown options
-		attributeDomainDropdown.append('<option value="0">String</option>');
-		attributeDomainDropdown.append('<option value="1">Char</option>');
-		attributeDomainDropdown.append('<option value="2">Integer</option>');
-		attributeDomainDropdown.append('<option value="3">Float</option>');
-		attributeDomainDropdown.append('<option value="4">Date</option>');
+		var attributeGroup = getAttributeGroup();
+		var attributeNameTextbox = getAttributeNameTextBox();
+		var attributeDomainDropdown = getDomainsDropdown();
 					
 		accordionContent.append(segment);
 		segment.append(attributeGroup);
@@ -55,7 +104,9 @@ function prepareSecondModal() {
 	}
 
 	// Initialize accordions
-	$(".ui.accordion").accordion();
+	$(".ui.accordion").accordion({
+		exclusive: true
+	});
 }
 
 $(document).ready(function() {
