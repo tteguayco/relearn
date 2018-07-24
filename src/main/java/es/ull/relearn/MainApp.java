f786b6ecc3d1b1b2d03dfdf6b106d8bfa5067861
tteguayco <teguayco95@gmail.com>
2018-07-24 15:17:10 +0000

    Serving html schema page from client request

--- src/main/java/es/ull/relearn/MainApp.java
+++ src/main/java/es/ull/relearn/MainApp.java
@@ -1,9 +1,28 @@
 package es.ull.relearn;
 import static spark.Spark.*;
 
+import java.io.IOException;
+
+import spark.Spark;
+import spark.utils.IOUtils;
+
 public class MainApp {
 
 	private static final int DEFAULT_PORT = 8080;
+	private static final String HOME_PAGE_PATH = "/views/home.html";
+	private static final String SCHEMA_PAGE_PATH = "/views/schema.html";
+	
+	private static String renderContent(String htmlFilePath) {
+		String htmlPageAsString = "";
+		
+		try {
+			htmlPageAsString = IOUtils.toString(Spark.class.getResourceAsStream(htmlFilePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return htmlPageAsString;
+	}
 	
 	public static void main(String[] args) {
 		
@@ -11,7 +30,7 @@ public class MainApp {
 		port(DEFAULT_PORT);
 		init();
 		
-		get("/", (req, res) -> "Showing home page");
-		get("/schema", (req, res) -> "Schema page!");
+		Spark.get("/", (req, res) -> renderContent(HOME_PAGE_PATH));
+		Spark.get("/schema", (req, res) -> renderContent(SCHEMA_PAGE_PATH));
 	}
 }
